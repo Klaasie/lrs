6b59041ab3167ac71ba47caef21d547d08153e2c
Klaas Poortinga <poortinga.k@gmail.com>
2015-09-20 18:45:44 +0200

    added messages component

---
+++ client/messages/helpers.js
@@ -0,0 +1,26 @@
+Template.messages.helpers({
+	classes: function(){
+		var classes;
+		var message = Session.get('messages');
+
+		switch (message.type){
+			case 'error':
+				classes = 'red darken-3';
+				break;
+			case 'success':
+			 	classes = 'light-green darken-1';
+			 	break;
+			case 'warning':
+				classes = 'yellow darken-1';
+				break;
+			default:
+				classes = 'blue darken-1';
+		}
+
+		return classes;
+	},
+	content: function(){
+		var message = Session.get('messages');
+		return message.content;
+	}
+});
