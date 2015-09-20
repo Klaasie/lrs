5814d6e4ffe92fc2f1e67e17210f95677bbb5402
Klaas Poortinga <poortinga.k@gmail.com>
2015-09-20 18:43:14 +0200

    improvements on sidemenu

--- client/navigation/sidemenu/events.js
+++ client/navigation/sidemenu/events.js
@@ -1,12 +1,27 @@
 Template.sidemenu.events({
 	"click .toggle-menu": function(event){
 		event.preventDefault();
+	},
+	"click #dashboard": function(event){
+		event.preventDefault();
+		$(".toggle-menu").sideNav('hide');
+		Router.go('/dashboard');
+	},
+	"click #stores": function(event){
+		event.preventDefault();
+		$(".toggle-menu").sideNav('hide');
+		Router.go('/stores');
+	},
+	"click #logout": function(event){
+		event.preventDefault();
+		$(".toggle-menu").sideNav('hide');
+		Meteor.logout();
 	}
 });
 
 Template.sidemenu.rendered = function(){
 	$(".toggle-menu").sideNav({
 		edge: 'left',
-		closeOnClick: true
+		// closeOnClick: true
 	});
 }
