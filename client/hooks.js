9c63480264905d210078f7ee7af05ea096b76ebf
Klaas Poortinga <poortinga.k@gmail.com>
2015-09-26 22:03:04 +0200

    added message

--- client/hooks.js
+++ client/hooks.js
@@ -1,5 +1,7 @@
 AutoForm.addHooks('insertAnswerForm', {
 	onSuccess: function(formType, result) {
+		Session.set('messages', {"content": "Learning Record Store added.", "type": "success"});
+
 		$('#addStore').closeModal();
 	}
 });
