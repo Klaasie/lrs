ca2446b560dd4dddfb3a436185eb609f6dd7b2d1
Klaas Poortinga <poortinga.k@gmail.com>
2015-09-27 20:03:39 +0200

    added link to specific store

--- client/stores/events.js
+++ client/stores/events.js
@@ -1,5 +1,10 @@
 Template.stores.events({
-	"click #openModal": function(){
+	"click #openModal": function(event){
+		event.preventDefault()
 		$('#addStore').openModal();
+	},
+	"click #goStore": function(event){
+		event.preventDefault();
+		Router.go('/stores/' + this._id);
 	}
 });
