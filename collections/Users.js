c8836d1f809375a4af3e722e41f432830785928a
Klaas Poortinga <poortinga.k@gmail.com>
2015-09-27 20:04:23 +0200

    if no username give email

--- collections/Users.js
+++ collections/Users.js
@@ -1,5 +1,9 @@
 Meteor.users.helpers({
 	fullName: function(){
+		if(typeof this.profile == "undefined"){
+			return this.emails[0].address;
+		}
+
 		return this.profile.firstname + ' ' + this.profile.lastname;
 	}
 });
