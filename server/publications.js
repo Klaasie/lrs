051f8667210a2b6908854485df25855026d4a057
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-03 15:10:25 +0200

    needed publication

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,11 @@ Meteor.publish('stores', function () {
 Meteor.publish("users", function () {
     return Meteor.users.find({});
 });
+
+Meteor.publish("statements", function (storeId){
+	check(storeId, String);
+
+	var store = Stores.findOne(storeId);
+
+	return Statements.find({_id: {$in: store.statements}});
+});
