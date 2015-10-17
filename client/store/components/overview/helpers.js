71126654913081a8ebaff486eea6d666f8efe96c
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-18 00:03:56 +0200

    Added activities overview

--- client/store/components/overview/helpers.js
+++ client/store/components/overview/helpers.js
@@ -31,5 +31,34 @@ Template.storeOverview.helpers({
 		var userCount = Meteor.users.find({statements: {$in: statementIds}}).count();
 
 		return userCount;
+	},
+	activityCount: function() {
+
+		// Retrieve statements
+		var statements = Statements.find({ _id: { $in: this.statements } }).fetch();
+
+		// Group them by object.id (the unique identifier)
+		var grouped = _.countBy(statements, function(statement){
+			return statement.object.id;
+		})
+
+		// Return count
+		return Object.keys(grouped).length;
+	},
+	activeActivities: function() {
+		// Get one week ago
+		var oneWeekAgo = new Date();
+		oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+		// Retrieve statements
+		var statements = Statements.find({ _id: { $in: this.statements }, stored: {$gt: oneWeekAgo.toISOString()} }).fetch();
+
+		// Group them by object.id (the unique identifier)
+		var grouped = _.countBy(statements, function(statement){
+			return statement.object.id;
+		})
+
+		// Return count
+		return Object.keys(grouped).length;
 	}
 });
