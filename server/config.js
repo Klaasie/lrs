90d3ca7f3167cac05480a371217fa89c3d13f48e
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-17 20:22:45 +0200

    I really will..

--- server/config.js
+++ server/config.js
@@ -24,5 +24,6 @@ Accounts.onCreateUser(function (options, user) {
 WebApp.connectHandlers.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   return next();
 });
