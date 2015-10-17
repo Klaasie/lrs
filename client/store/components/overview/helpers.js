021df9b4bc822cd6a68186c5390c9f41da3a3176
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-17 23:00:44 +0200

    component moved with the store

---
+++ client/store/components/overview/helpers.js
@@ -0,0 +1,35 @@
+Template.storeOverview.helpers({
+	statementsCount: function(){
+		if(typeof this.statements != "undefined"){
+			return this.statements.length;
+		} else {
+			return 0;
+		}
+	},
+	statementsLastWeek: function(){
+		// Get one week ago
+		var oneWeekAgo = new Date();
+		oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+		
+		// Query amount
+		var statementsCount = Statements.find({stored: {$gt: oneWeekAgo.toISOString()}}).count();
+
+		// Return it
+		return statementsCount;
+	},
+	usersCount: function() {
+		return this.users.length;
+	},
+	activeUsers: function() {
+		// Get one week ago
+		var oneWeekAgo = new Date();
+		oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+		
+		// Query amount
+		var statementIds = Statements.find({stored: {$gt: oneWeekAgo.toISOString()}}).map(function(item){ return item._id; });
+
+		var userCount = Meteor.users.find({statements: {$in: statementIds}}).count();
+
+		return userCount;
+	}
+});
