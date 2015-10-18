368a37ff81f91b9801b83aad8225e0becc7641aa
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-18 15:24:50 +0200

    modified store page to have an active store

--- client/stores/events.js
+++ client/stores/events.js
@@ -6,5 +6,10 @@ Template.stores.events({
 	"click #goStore": function(event){
 		event.preventDefault();
 		Router.go('/store/' + this._id);
+	},
+	"click #activate": function(event){
+		event.preventDefault();
+
+		Meteor.call('activateStore', this._id);
 	}
 });
--- client/stores/helpers.js
+++ client/stores/helpers.js
@@ -1,5 +1,23 @@
 Template.stores.helpers({
-	stores: function(){
+	stores: function() {
 		return Stores.find();
+	},
+	statusClass: function() {
+		var user = Meteor.user();
+
+		if(user.profile.activeStore == this._id){
+			return "green white-text";
+		}
+
+		return "";
+	},
+	status: function() {
+		var user = Meteor.user();
+
+		if(user.profile.activeStore == this._id){
+			return "Active";
+		}
+
+		return "Inactive";
 	}
 });
