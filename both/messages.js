37b49b9c9b0ae4c7fbac189c1b4a183912e860c1
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-18 16:29:34 +0200

    added documentation

--- both/messages.js
+++ both/messages.js
@@ -4,12 +4,25 @@
  */
 Messages = function(){
 
+	/**
+	 * Box to contain all messages
+	 * @type {Array}
+	 */
 	this.box = [];
 
+	/**
+	 * Retrieve all messages
+	 * @return {Array} Array of objects
+	 */
 	this.get = function(){
 		return this.box;
 	}
 
+	/**
+	 * Set a message
+	 * @param {String} text Contains message text
+	 * @param {String} type Contains message type
+	 */
 	this.set = function(text, type){
 
 		var id = this.box.length + 1;
@@ -21,6 +34,10 @@ Messages = function(){
 		this.box[id].type = type;
 	}
 
+	/**
+	 * Remove message from box
+	 * @param  {Integer} id Id of message to remove
+	 */
 	this.remove = function(id) {
 		this.box.splice(id, 1);
 	}
