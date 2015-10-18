5f36ead041f0aac08440eb59995e517996c7695d
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-18 15:48:27 +0200

    Mobile settings

---
+++ mobile-config.js
@@ -0,0 +1,15 @@
+App.info({
+  id: 'frl.klaas.lrs',
+  name: 'LRS',
+  description: 'Klaas Poortinga LRS',
+  author: 'Klaas Poortinga',
+  email: 'lrs@klaas.frl',
+  website: 'http://klaas.frl'
+});
+
+// Set up external locations the app needs access to.
+App.accessRule("*", {external: false});
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#000000');
+App.setPreference("orientation", "portrait");
