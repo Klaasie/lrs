05b3e158396448b1aa6c518c13cc39c2a8e2bcc9
Klaas Poortinga <poortinga.k@gmail.com>
2015-10-20 19:19:01 +0200

    Stores overview page working better now

--- client/stores/events.js
+++ client/stores/events.js
@@ -3,13 +3,40 @@ Template.stores.events({
 		event.preventDefault()
 		$('#addStore').openModal();
 	},
-	"click #goStore": function(event){
+	"click .collection-item": function(event){
 		event.preventDefault();
 		Router.go('/store/' + this._id);
 	},
+	"click .editStore": function(event){
+		event.preventDefault();
+		event.stopPropagation(); // Prevent going to the store
+	},
 	"click #activate": function(event){
 		event.preventDefault();
+		event.stopPropagation(); // Prevent going to the store
 
 		Meteor.call('activateStore', this._id);
+	},
+	"click .storesActivate": function(event){
+		event.preventDefault();
+		event.stopPropagation(); // Prevent going to the store
+
+		Meteor.call('activateStore', this._id);
+	},
+	"click .storesEdit": function(event){
+		event.preventDefault();
+		event.stopPropagation(); // Prevent going to the store
+
+		//Meteor.call('activateStore', this._id);
+	},
+	"click .storesRemove": function(event){
+		event.preventDefault();
+		event.stopPropagation(); // Prevent going to the store
+
+		//Meteor.call('activateStore', this._id);
 	}
 });
+
+Template.stores.rendered = function(){
+	$('.editStore').dropdown();
+}
